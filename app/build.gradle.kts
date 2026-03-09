8988713b9693205003d8e55448ee1baa09991c73
apollocked <hamabarznji1990@gmail.com>
2026-03-09 04:19:50 +0300

    Create the Main page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,13 @@ android {
 }
 
 dependencies {
+
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
