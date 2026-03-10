1b9fce7baf9a66976bd03f1f4f5c7514e219dfb4
apollocked <hamabarznji1990@gmail.com>
2026-03-10 03:15:41 +0300

    Feature : adiing settings Page

---
+++ app/src/main/java/com/example/weatherapp/WeatherApplication.kt
@@ -0,0 +1,19 @@
+package com.example.weatherapp
+
+import android.app.Application
+import android.content.Context
+import androidx.appcompat.app.AppCompatDelegate
+
+class WeatherApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        val sharedPref = getSharedPreferences("WeatherPrefs", Context.MODE_PRIVATE)
+        val isDarkMode = sharedPref.getBoolean("darkMode", false)
+        
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+    }
+}
